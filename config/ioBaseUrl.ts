2b9573325e35f49e80eb6a5f32e02fe5ab0a3e6a
hmzsumon <zsumonn@gmail.com>
2025-01-02 22:07:38 +0600

    first commit

--- config/ioBaseUrl.ts
+++ config/ioBaseUrl.ts
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
 	ioBaseUrl = 'http://localhost:8000';
 	console.log('Running in development mode');
 } else {
-	ioBaseUrl = 'https://icm-api-7808882a0bab.herokuapp.com';
+	ioBaseUrl = 'https://icm-api-25-747647fc7f34.herokuapp.com';
 	// Code specific to production mode
 	console.log('Running in production mode');
 }
