38783fb58cc4ef64675827fa29a70e1d26baba5e
hmzsumon <zsumonn@gmail.com>
2025-02-26 21:28:56 +0600

    first commit

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -338,6 +338,18 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 															Fund Transfer
 														</Link>
 													</li>
+
+													<li>
+														<Link
+															href='/transactions'
+															className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
+																pathname === '/wallet/transactions' &&
+																'text-white'
+															}`}
+														>
+															Fund Reports
+														</Link>
+													</li>
 												</ul>
 											</div>
 											{/* <!-- Dropdown Menu End --> */}
